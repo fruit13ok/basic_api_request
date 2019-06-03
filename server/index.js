6dfa9b42d299aefe001a217c977d5e1831f394cf
fruit13ok <fruit13ok@gmail.com>
2019-06-03 16:44:05 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,77 @@
+// REQUIREMENTS
+// native
+const path = require('path')
+// 3rd party
+const express = require('express')
+const bodyParser = require('body-parser')
+const fetch = require("node-fetch");
+
+// local
+const app = express()
+const port = process.env.PORT || 8000
+
+// MIDDLEWARE
+// either way will point to static folder to find index.html
+// run script will start server/index.js
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+// allow cors to access this backend
+app.use( (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// INIT SERVER
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+})
+
+// helper functions
+// return JSON data or error object, request weather api
+// I use helper function, optional, you can do request inside the route
+const getWeather = async (url) => {
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        return json;
+    }catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+// ROUTES
+// root
+app.get('/', function (req, res) {
+    res.send('hello world');
+    // this file structure index.js is not on top folder, so don't use direct file
+    // res.sendFile(__dirname + '/public/index.html');
+})
+
+// 3). Back end request Weather API
+// GET route
+// shows the json data
+// api end point I use, go to this link for instruction
+// https://fcc-weather-api.glitch.me/
+// I created a helper function getWeather(url) to fetch data
+app.get('/api', async function (req, res) {
+    // I use GET request here, if you like to use POST request please change the code
+    //      backend request weather data
+    //          if success
+    //              pass JSON data as response to front end
+    //          if error
+    //              pass a message as response to front end
+
+    let latitude = req.query.latitude;
+    let longitude = req.query.longitude;
+    let url = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
+    let data = await getWeather(url);
+    if(data){
+        res.status(200).json(data);
+    }else{
+        res.status(400).json("error has occured");
+    }
+})
---
+++ solution/app.js
@@ -0,0 +1,85 @@
+// front end js interact with html and backend
+console.log("Sanity Check: JS is working!");
+
+// access DOM contents
+let lat = document.getElementById("latitude");
+let lon = document.getElementById("longitude");
+let bac = document.getElementById("backResponse");
+let wea = document.getElementById("weather");
+let tem = document.getElementById("temperature");
+let cit = document.getElementById("city");
+let cou = document.getElementById("country");
+
+let backendRoute = new URL("http://localhost:8000/api");
+
+// 1). Front end request geolocation
+// get geolocation latitude longitude
+// need to enable browser location setting
+const getCurLocation = () => {
+    // request for geolocation, 
+    // if success
+    //      extract latitude and longitude from response
+    //          append latitude and longitude to html
+    //      call getWeather(backendRoute) and pass in latitude and longitude
+    //      I use GET request pass data by query-string, 
+    //      If you use POST request you can change the given codes.
+    // if error 
+    //      append error message to latitude and longitude to html
+    const success = (position) => {
+        const latitude  = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        lat.innerHTML += latitude;
+        lon.innerHTML += longitude;
+        // attach query-string, either way works, I like second one is more dynamic
+        // getWeather(`http://localhost:8000/api?latitude=${latitude}&longitude=${longitude}`);
+        params = {latitude:latitude, longitude:longitude};
+        Object.keys(params).forEach(key => backendRoute.searchParams.append(key, params[key]));
+        getWeather(backendRoute);
+    }
+    const error = () => {
+        lat.innerHTML += 'Unable to retrieve your location';
+        lon.innerHTML += 'Unable to retrieve your location';
+    }
+    navigator.geolocation.getCurrentPosition(success, error);
+}
+
+// 2). Front end request to back end
+// 3). Back end request Weather API
+// get weather forecast of local city
+// front end I send GET request to back end, if you use POST request you can change given codes
+// if native fetch not working try to install / use node-fetch or ajax or axios
+// backend should already allowed CORS, if not then install / use CORS
+const getWeather = async (backendRoute) => {
+    // request the backend route, pass in latitude and longitude
+    // if request success
+    //      append a backend response success message to html
+    //      parse backend response
+    //      append weather data to html
+    //  if error
+    //      append a backend response error message to html
+    //      append error message to weather data to html
+    try {
+        const response = await fetch(backendRoute);
+        if(response){
+            bac.innerHTML += ' backend response success';
+        }
+        const json = await response.json();
+        let weather = json.weather[0].main;
+        let temperature = json.main.temp;
+        let city = json.name;
+        let country = json.sys.country;
+        wea.innerHTML += weather;
+        tem.innerHTML += temperature + ' &#176;C';
+        cit.innerHTML += city;
+        cou.innerHTML += country;
+    }catch (error) {
+        bac.innerHTML += ' backend response fail';  // when wrong url
+        wea.innerHTML += error;
+        tem.innerHTML += error;
+        cit.innerHTML += error;
+        cou.innerHTML += error;
+    }
+}
+
+// start the request
+getCurLocation();
